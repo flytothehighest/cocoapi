274dd6fd72450fac98313eefc03f27d77db6d530
Explore114 <19877809391@163.com>
2025-07-20 21:28:02 +0800

    站点监测测试

--- uptime-status/config.js
+++ uptime-status/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Duococo-cocoapi网站状态检测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm800987860-224e15a0ffd5e2bb3ffce137',
   ],
 
   // 日志天数
@@ -21,16 +18,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Duococo-cocoapi首页',
+      url: 'https://cocoapi.codejun.top'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://github.com/duococo/cocoapi'
     },
   ],
 };
